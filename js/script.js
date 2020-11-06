09c857a3fc9c906da8e04d65fe9ddf765d03e050
Andrea Bevoni <andreabevoni@users.noreply.github.com>
2020-11-06 21:48:56 +0100

    Simon says: giro dei colori perfettamente funzionante

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+// Variante del Simon Says simile all'originale
+
 $(document).ready(function () {
 
+  // definisco le variabili globali che mi servono
   var arraySimon = [];
 
   // il gioco comincia quando viene premuto il pulsante
@@ -9,19 +12,24 @@ $(document).ready(function () {
       // creo un numero random fra 0 e 3 e lo inserisco nell'array
       arraySimon.push(Math.floor(Math.random()*4) + 1);
 
-      console.log(arraySimon);
-
+      // utilizzo un countdown per mostrare le mosse da fare ad intervalli regolari
       var i = 0;
-
-      var countdown = setInterval(simonSays, 500);
-
+      var countdown = setInterval(simonSays, 400);
       function simonSays() {
+
         if (i < arraySimon.length) {
+
+          // mi salvo la variabile di controllo perché questa viene modificata prima che parta il timeout che toglie la classe
           var x = i;
+
+          // aggiungo la classe active per mostrare la mossa
           $(".ring:nth-child(" + arraySimon[i] + ")").addClass("active");
+
+          // tolgo la classe active dopo un piccolo intervallo (deve essere inferiore a quello del countdown o non funziona)
           setTimeout(function(){
             $(".ring:nth-child(" + arraySimon[x] + ")").removeClass("active");
-          },500);
+          },200);
+
           i++;
         } else {
           clearInterval(countdown);
